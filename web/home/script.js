1b8b051a9128ef80843e0c812fec2bcfbd47bcb5
shivzee <wizzard1608@gmail.com>
2022-12-02 07:28:12 +0530

    Added: Homepage Template

--- web/home/script.js
+++ web/home/script.js
@@ -1,1 +1,7 @@
-console.log("Loaded");
+const openGithub = () => {
+  window.location = "https://github.com/shivam1608/badwords";
+};
+
+window.onload = () => {
+  document.getElementsByClassName("hidden-text")[0].classList.remove("hidden");
+};
