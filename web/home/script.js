9952432aa56963fc7cf3ac1e02d974b7f132679b
shivzee <wizzard1608@gmail.com>
2022-12-04 23:40:01 +0530

    Fixed: Homepage demo request

--- web/home/script.js
+++ web/home/script.js
@@ -6,14 +6,18 @@ const openGithub = () => {
 const sendDemoRequest = async (req) => {
   let time = Date.now();
   let response = await fetch("/api/clean" , {
+    headers : {
+      "Content-Type" : "application/json"
+    },
     method : "POST",
-    body : JSON.stringify(JSON.parse(req)) , 
+    body : req , 
   });
   response = await response.json();
   response = response.value;
-  time-=Date.now();
-  time+="ms"
-  return {response , time};
+  time=(Date.now()-time)+"ms"
+  return new Promise((resolve)=>{
+    resolve({response , time});
+  });
 }
 
 window.onload = () => {
