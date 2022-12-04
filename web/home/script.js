ff8b5bd5f80951acc35e863ca6318ca4f0dc2fe0
shivzee <wizzard1608@gmail.com>
2022-12-04 23:22:04 +0530

    Added: Demo requests on homepage

--- web/home/script.js
+++ web/home/script.js
@@ -2,6 +2,20 @@ const openGithub = () => {
   window.location = "https://github.com/shivam1608/badwords";
 };
 
+
+const sendDemoRequest = async (req) => {
+  let time = Date.now();
+  let response = await fetch("/api/clean" , {
+    method : "POST",
+    body : JSON.stringify(JSON.parse(req)) , 
+  });
+  response = await response.json();
+  response = response.value;
+  time-=Date.now();
+  time+="ms"
+  return {response , time};
+}
+
 window.onload = () => {
   document.getElementsByClassName("hidden-text")[0].classList.remove("hidden");
 };
