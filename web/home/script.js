bc1ee0f90354b5013d4cfaeb33109b2902053129
shivzee <64302819+shivam1608@users.noreply.github.com>
2022-12-11 20:05:16 +0000

    Added: Number format on homepage

--- web/home/script.js
+++ web/home/script.js
@@ -2,6 +2,28 @@ const openGithub = () => {
   window.location = "https://github.com/shivam1608/badwords";
 };
 
+const intToString = (num) => {
+  num = num.toString().replace(/[^0-9.]/g, '');
+  if (num < 1000) {
+      return num;
+  }
+  let si = [
+    {v: 1E3, s: "K"},
+    {v: 1E6, s: "M"},
+    {v: 1E9, s: "B"},
+    {v: 1E12, s: "T"},
+    {v: 1E15, s: "P"},
+    {v: 1E18, s: "E"}
+    ];
+  let index;
+  for (index = si.length - 1; index > 0; index--) {
+      if (num >= si[index].v) {
+          break;
+      }
+  }
+  return (num / si[index].v).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") + si[index].s;
+}
+
 const sendDemoRequest = async (req) => {
   let time = Date.now();
   let response = await fetch("/api/clean", {
@@ -22,7 +44,7 @@ const sendDemoRequest = async (req) => {
 const getApiInfo = async () => {
   let response = await fetch("/api");
   response = await response.json();
-  let totalwords = response.data.totalwords;
+  let totalwords = intToString(response.data.totalwords);
   let total = response.data.total;
   return new Promise((resolve) => {
     resolve({ totalwords, total });
